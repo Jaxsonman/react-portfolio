f3ac9b79568b2c55eee65fe576afc9c8cd40a34c
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2020-05-12 14:13:13 -0600

    lots of changes

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+import PortfolioItem from './portfolio-item';
+
+export default class PortfolioContainer extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio.",
+            isLoading: false,
+            data: []
+        }
+        this.handleFilter = this.handleFilter.bind(this)
+    }
+
+    getPortfolioItems(){
+        axios
+          .get('https://jaxsonmansouri.devcamp.space/portfolio/portfolio_items')
+          .then(response => {
+            console.log(response)
+            this.setState({
+                data: response.data.portfolio_items
+            })
+          })
+          .catch(error =>{
+            console.log(error)
+          })
+      }
+
+    portfolioItems(){
+        return this.state.data.map(item => {
+            return <PortfolioItem key={item.id} item={item}/>;
+        })
+    }
+
+    handleFilter(filter){
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        })
+    }
+
+    componentDidMount(){
+        this.getPortfolioItems();
+    }
+
+    render(){
+        if(this.state.isLoading) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div>
+                <h2>{this.state.pageTitle}</h2>
+                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                <button onClick={() => this.handleFilter('learning')}>Learning</button>
+                <button onClick={() => this.handleFilter('CRM')}>CRM</button>
+
+                <div className="portfolio-items-wrapper">
+                    {this.portfolioItems()}
+                </div>
+            </div>
+        )
+    }
+}
+
